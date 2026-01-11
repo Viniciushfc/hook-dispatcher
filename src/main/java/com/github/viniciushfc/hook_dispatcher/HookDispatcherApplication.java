03d31d1644e9b89b004a4c70b5a50f665a48b17b
viniciushfc <vinicius.ferrari@gmail.com>
2026-01-11 14:28:01 -0300

    first commit project

---
+++ src/main/java/com/github/viniciushfc/hook_dispatcher/HookDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciushfc.hook_dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookDispatcherApplication.class, args);
+	}
+
+}
