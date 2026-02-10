e5665285b3bd7729e361d9c5bfc1e62b5ea106ec
viniciushfc <vinicius.henry.fc@gmail.com>
2026-02-09 21:33:21 -0300

    refactor(service): Foi trocado retorno de Entidade para DTO nos metodos abstracts

--- src/main/java/com/github/viniciushfc/hook_dispatcher/application/service/generic/IBaseService.java
+++ src/main/java/com/github/viniciushfc/hook_dispatcher/application/service/generic/IBaseService.java
@@ -2,17 +2,16 @@ package com.github.viniciushfc.hook_dispatcher.application.service.generic;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 
-public interface IBaseService<ID, E, D> {
+public interface IBaseService<ID, D> {
 
-    E save(D dto);
+    D save(D dto);
 
-    Page<E> findAll(Pageable pageable, Specification<E> spec);
+    Page<D> findAll(Pageable pageable);
 
-    E findById(ID id);
+    D findById(ID id);
 
-    E update(ID id, D dto);
+    D update(ID id, D dto);
 
     String delete(ID id);
 }
